538316fef4f871c97085fa120b3b7055e940bd41
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-03-14 14:22:32 +0100

    Adding some comments how the websocket example works

--- websockets-01/app/app.js
+++ websockets-01/app/app.js
@@ -1,12 +1,25 @@
+// Create a connection to the WebSocket server running on localhost:8080
 const socket = io("ws://localhost:8080");
 
+// Listen for incoming "message" events from the WebSocket server
 socket.on("message", (text) => {
+  // Create a new <li> element for each incoming message
   const el = document.createElement("li");
+
+  // Set the content of the <li> element to the received message (text)
   el.innerHTML = text;
+
+  // Append the new <li> element to the <ul> on the webpage
+  // This will add the message to the list of messages shown to the user
   document.querySelector("ul").appendChild(el);
 });
 
+// Add an event listener for when the user clicks the "button" on the webpage
 document.querySelector("button").onclick = () => {
+  // Get the text entered by the user in the <input> field
   const text = document.querySelector("input").value;
+
+  // Send the text as a "message" event to the WebSocket server
+  // The message will be broadcast to all connected clients
   socket.emit("message", text);
 };
--- websockets-01/server/index.js
+++ websockets-01/server/index.js
@@ -1,16 +1,29 @@
+// Import the HTTP module to create an HTTP server.
 const http = require("http").createServer();
 
+// Import the socket.io module to enable WebSocket communication.
 const io = require("socket.io")(http, {
-  cors: { origin: "*" },
+  cors: { origin: "*" }, // Allow connections from any origin (for simplicity)
 });
 
+// This event triggers when a new user (socket) connects to the server.
 io.on("connection", (socket) => {
   console.log("a user connected");
 
+  // This event listens for the "message" event from the client.
   socket.on("message", (message) => {
-    console.log(message);
+    console.log(message); // Log the received message on the server.
+
+    // Broadcast the message to all connected clients.
+    // `socket.id` is used to identify the sender.
     io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+  });
+
+  // This is an optional event that can be triggered when the socket disconnects.
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
   });
 });
 
+// Start the HTTP server and listen on port 8080.
 http.listen(8080, () => console.log("listening on http://localhost:8080"));
